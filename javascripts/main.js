66d61c63ab253df47ef21fb23205b783b7487314
Your Name <you@example.com>
2026-03-15 17:24:43 -0400

    add svg support

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,9 +13,25 @@ convertButton.onclick = function () {
     [].forEach.call(fileInput.files, function (file) {
         var reader = new FileReader();
         reader.addEventListener('load', function (event) {
-            var font = opentype.parse(event.target.result);
-            var result = convert(font);
-            exportString(result, font.names.fontFamily.en + "_" + font.names.fontSubfamily.en + ".json");
+            let result;
+
+            if (file.type == "font/ttf") {
+                var font = opentype.parse(event.target.result);
+                result = convert(font);
+            } else if (file.type == "image/svg+xml") {
+                var decoder = new TextDecoder('utf-8');
+                var str = decoder.decode(event.target.result);
+                const parser = new DOMParser()
+                const obj = parser.parseFromString(str, "image/svg+xml");
+                result = convertSvg(obj);
+
+            } else {
+                alert("Unsupported file type: " + file.type);
+                return;
+            }
+
+            exportString(JSON.stringify(result), result.fontFamily + "_" + result.fontSubfamily + ".json");
+
         }, false);
         reader.readAsArrayBuffer(file);
     });
@@ -74,7 +90,7 @@ var convert = function (font) {
         }
     }
 
-    var glyphs = Array(font.glyphs.length).fill(0).map((e,i) => font.glyphs.get(i));
+    var glyphs = Array(font.glyphs.length).fill(0).map((e, i) => font.glyphs.get(i));
     glyphs.forEach(function (glyph) {
         const unicodes = [];
         if (glyph.unicode !== undefined) {
@@ -129,13 +145,13 @@ var convert = function (font) {
         });
     }
 
-    var firstOr = (arr,  def) => arr.length == 1 ? arr[0] : def;
+    var firstOr = (arr, def) => arr.length == 1 ? arr[0] : def;
     result.gposPair = (font.tables.gpos && font.tables.gpos.lookups) ? firstOr(font.tables.gpos.lookups.filter(e => e.lookupType == 2)) : undefined;
     result.familyName = font.familyName;
     result.ascender = (font.ascender * scale);
     result.descender = (font.descender * scale);
     result.underlinePosition = (font.tables.post.underlinePosition * scale);
-    
+
     result.underlineThickness = (font.tables.post.underlineThickness * scale);
     result.boundingBox = {
         "yMin": (font.tables.head.yMin * scale),
@@ -157,8 +173,139 @@ var convert = function (font) {
         result.cssFontStyle = "normal";
     };
 
-    return JSON.stringify(result);
+    result.fontFamily = font.names.fontFamily.en;
+    result.fontSubfamily = font.names.fontSubfamily.en;
+
+    return result;
 };
+
+var xmlToObj = function (xml) {
+    var obj = {};
+    obj.tag = xml.tagName ?? xml.nodeName;
+    obj.attrs = {};
+    _.forEach(xml.attributes, (v) => {
+        obj.attrs[v.name] = v.value;
+    });
+    obj.children = [];
+    _.forEach(xml.children, (v) => {
+        obj.children.push(xmlToObj(v))
+    });
+    return obj;
+};
+
+var pathToObjs = function (glyph, str) {
+    let objs = [];
+    if (typeof str !== 'string')
+        return objs;
+    let els = _(str.split(/([mMzZlLqQcC ])/)).filter(v => v != '' && v != ' ');
+    let next = () => { let x = els.next(); return !x.done ? x.value : undefined; };
+    let nextNum = () => Math.round(Number(next()));
+    let cmd;
+    let cur = [0, 0];
+    while ((cmd = next())) {
+        // lower case is relative, upper case is absolute
+        // convert commands to absolute
+        let offset = cmd == cmd.toUpperCase() ? [0, 0] : cur;
+        cmd = cmd.toUpperCase();
+        if (cmd == 'M') {
+            let x = nextNum() + offset[0];
+            let y = nextNum() + offset[1];
+            objs.push({ 'type': cmd, 'x': x, 'y': y });
+            cur = [x, y];
+        } else if (cmd == 'Z') {
+            objs.push({ 'type': cmd });
+            cur = [0, 0];
+        } else if (cmd == 'L') {
+            let x = nextNum() + offset[0];
+            let y = nextNum() + offset[1];
+            objs.push({ 'type': cmd, 'x': x, 'y': y });
+            cur = [x, y];
+        } else if (cmd == 'Q') {
+            let x1 = nextNum() + offset[0];
+            let y1 = nextNum() + offset[1];
+            let x = nextNum() + offset[0];
+            let y = nextNum() + offset[1];
+            objs.push({ 'type': cmd, 'x': x, 'y': y, 'x1': x1, 'y1': y1 });
+            cur = [x, y];
+        } else if (cmd == 'C') {
+            let x1 = nextNum() + offset[0];
+            let y1 = nextNum() + offset[1];
+            let x2 = nextNum() + offset[0];
+            let y2 = nextNum() + offset[1];
+            let x = nextNum() + offset[0];
+            let y = nextNum() + offset[1];
+            objs.push({ 'type': cmd, 'x': x, 'y': y, 'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2 });
+            cur = [x, y];
+        } else {
+            throw "Invalid cmd '" + cmd + "' for glyph '" + glyph + "'";
+        }
+    }
+    return objs;
+}
+
+var convertSvg = function (svgXml) {
+    var svg = xmlToObj(svgXml);
+    var defs = _.findValueDeep(svg, (v, k, p) => v.tag == 'defs', { childrenPath: 'children' });
+    if (!defs)
+        throw "SVG doesn't contain fonts";
+
+    var fonts = _.filter(defs.children, v => v.tag == 'font');
+    if (fonts.length == 0)
+        throw "SVG doesn't contain fonts";
+    if (fonts.length > 1)
+        alert('SVG contains more than 1 font. Only converting first font');
+    let font = fonts[0];
+
+    var fontFace = _.find(font.children, v => v.tag == 'font-face');
+    if (!fontFace)
+        throw "SVG doesn't contain font-face";
+
+    let result = {};
+    result.glyphs = {};
+    result.charMap = {};
+    result.indexMap = {};
+
+    _(font.children)
+        .filter(v => v.tag == 'glyph')
+        .forEach((v, i) => {
+            let token = {};
+            let unicode = v.attrs.unicode.charCodeAt(0);
+            let glyphCharacter = v.attrs.unicode[0];
+
+            token.ha = Number(v['horiz-adv-x'] ?? font.attrs['horiz-adv-x']);
+            token.objs = pathToObjs(glyphCharacter, v.attrs.d);
+            let xs = _.flatMap(token.objs, o => [o.x, o.x1, o.x2]);
+            let ys = _.flatMap(token.objs, o => [o.y, o.y1, o.y2]);
+            let minX = _.min(xs);
+            let minY = _.min(ys);
+            let maxX = _.max(xs);
+            let maxY = _.max(ys);
+            token.x_min = minX;
+            token.y_min = minY;
+            token.x_max = maxX;
+            token.y_max = maxY;
+            result.charMap[glyphCharacter] = String(unicode);
+            result.indexMap[i] = String(unicode);
+            result.glyphs[unicode] = token;
+        });
+
+    result.ascender = Number(fontFace.attrs.ascent);
+    result.descender = Number(fontFace.attrs.descent);
+    result.underlinePosition = fontFace.attrs['underline-position'] ? Number(fontFace.attrs['underline-position']) : undefined;
+    result.underlineThickness = fontFace.attrs['underline-thickness'] ? Number(fontFace.attrs['underline-thickness']) : undefined;
+
+    result.boundingBox = {
+        "xMin": _.min(_.map(result.glyphs, v => v.x_min)),
+        "yMin": _.min(_.map(result.glyphs, v => v.y_min)),
+        "xMax": _.max(_.map(result.glyphs, v => v.x_max)),
+        "yMax": _.max(_.map(result.glyphs, v => v.y_max)),
+    };
+    result.resolution = Number(fontFace.attrs['units-per-em']);
+    result.fontSubfamily = "normal";
+    result.fontFamily = fontFace.attrs['font-family'];
+
+    return result;
+}
 
 var reverseCommands = function (commands) {
 
